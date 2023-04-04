6caee002d398f9fb1d6a0118c4c96b2696dff970
sewon.oh <126149375+sewon-O@users.noreply.github.com>
2023-04-04 17:10:37 +0900

    Add files via upload

---
+++ 20230404/003_sort.js
@@ -0,0 +1,30 @@
+// 실무사용코드
+function sort(key) {
+  if (click) {
+    click = false;
+    var sortedData = jsonData.sort((a, b) =>
+      a[key] < b[key] ? -1 : a[key] > b[key] ? 1 : 0
+    );
+  } else {
+    click = true;
+    var sortedData = jsonData.sort((a, b) =>
+      a[key] > b[key] ? -1 : a[key] < b[key] ? 1 : 0
+    );
+  }
+}
+
+//
+const nums = [3, 1, 11, 8, 6];
+console.log(
+  nums.sort((a, b) => {
+    console.log(a, b);
+  })
+);
+
+const nums = [3, 1, 11, 8, 6];
+console.log(
+  nums.sort((a, b) => {
+    console.log(a, b);
+    console.log(a - b);
+  })
+);
