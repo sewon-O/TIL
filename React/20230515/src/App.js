9c4640f9f19260e41e91e7c4dcf03f1bebba6d0a
sewon.oh <126149375+sewon-O@users.noreply.github.com>
2023-05-15 17:04:15 +0900

    Add files via upload

---
+++ React/20230515/src/App.js
@@ -0,0 +1,20 @@
+import RouteSolve from "./RouterSolve/RouteSolve";
+import Counter from "./HookUserEffect/TryUseEffect";
+import Time from "./HookUserEffect/Quiz2";
+import Counter2 from "./HookUseRef/TryUseRef";
+import TryUseRefDom from "./HookUseRef/TryUseRefDom";
+import TryUseMemo from "./HookUseMemo/TryUseMemo";
+
+function App() {
+  return (
+    <div>
+      {/* <RouteSolve /> */}
+      {/* <Counter /> */}
+      {/* <Time /> */}
+      {/* <Counter2 /> */}
+      {/* <TryUseRefDom /> */}
+      <TryUseMemo />
+    </div>
+  );
+}
+export default App;
